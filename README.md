a10006e13ef0a0729e004391d72b552233a15752
Sebastian Cabeza <sebastian.cabeza@noted.com>
2021-08-16 10:38:13 +1200

    Initialise project

---
+++ README.md
@@ -0,0 +1,3 @@
+## Lucía and Sebastián are getting married
+
+Please, visit our [wedding site](https://casamiento.kiwi).
---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+export const App = () => {
+  return (
+    <div>
+      <header>Lucía & Sebastián</header>
+    </div>
+  );
+};
+
+export default App;
