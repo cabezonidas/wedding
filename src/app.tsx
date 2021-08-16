e3d9926cc824df149f084f989f5c9d08b36c8142
Sebastian Cabeza <sebastian.cabeza@noted.com>
2021-08-16 13:11:57 +1200

    Update message

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,43 @@
 import * as React from "react";
 export const App = () => {
+  const height = useHeight();
   return (
-    <div>
-      <header>Lucía & Sebastián</header>
+    <div
+      style={{
+        height,
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        textAlign: "center",
+      }}
+    >
+      <div>
+        <div>Save the date</div>
+        <div>for the wedding of</div>
+        <header>Lucía & Sebastián</header>
+        <div style={{ fontSize: "30px" }}>💍</div>
+      </div>
     </div>
   );
 };
+
+function useHeight() {
+  const [height, setHeight] = React.useState(
+    window.innerHeight * (window.visualViewport?.scale || 1)
+  );
+
+  React.useEffect(() => {
+    const onResize = () => {
+      setHeight(window.innerHeight * (window.visualViewport?.scale || 1));
+    };
+
+    window.addEventListener("resize", onResize);
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  });
+
+  return height + "px";
+}
 
 export default App;
