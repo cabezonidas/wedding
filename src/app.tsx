b8eec40bda453367d7fb58b498067c13dc225d9f
Sebastian Cabeza <sebastian.cabeza@noted.com>
2021-10-28 11:36:56 +1300

    Add flyer

--- src/app.tsx
+++ src/app.tsx
@@ -1,43 +1,35 @@
 import * as React from "react";
+import flyer from "./flyer.jpeg";
+import background from "./background.svg";
+
 export const App = () => {
-  const height = useHeight();
   return (
     <div
       style={{
-        height,
         display: "flex",
         justifyContent: "space-around",
         alignItems: "center",
         textAlign: "center",
+        backgroundImage: `url("${background}")`,
       }}
     >
-      <div>
-        <div>Save the date</div>
-        <div>for the wedding of</div>
-        <header>Lucía & Sebastián</header>
-        <div style={{ fontSize: "30px" }}>💍</div>
+      <div
+        style={{
+          maxWidth: 700,
+          border: "20px solid white",
+          margin: "50px 10px 50px 10px",
+          boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+          display: "flex",
+        }}
+      >
+        <img
+          src={flyer}
+          alt="8th January 2022, save the date"
+          style={{ height: "100%", width: "100%" }}
+        />
       </div>
     </div>
   );
 };
-
-function useHeight() {
-  const [height, setHeight] = React.useState(
-    window.innerHeight * (window.visualViewport?.scale || 1)
-  );
-
-  React.useEffect(() => {
-    const onResize = () => {
-      setHeight(window.innerHeight * (window.visualViewport?.scale || 1));
-    };
-
-    window.addEventListener("resize", onResize);
-    return () => {
-      window.removeEventListener("resize", onResize);
-    };
-  });
-
-  return height + "px";
-}
 
 export default App;
