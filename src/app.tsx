e156b4fd6016f6c6ec93736712ca5e664d0bc148
Sebastian Cabeza <sebastian.cabeza@noted.com>
2021-10-28 15:06:34 +1300

    Add RSVP

--- src/app.tsx
+++ src/app.tsx
@@ -11,6 +11,7 @@ export const App = () => {
         alignItems: "center",
         textAlign: "center",
         backgroundImage: `url("${background}")`,
+        flexDirection: "column",
       }}
     >
       <div
@@ -20,6 +21,9 @@ export const App = () => {
           margin: "50px 10px 50px 10px",
           boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
           display: "flex",
+          position: "relative",
+          flexDirection: "column",
+          background: "white",
         }}
       >
         <img
@@ -27,6 +31,25 @@ export const App = () => {
           alt="8th January 2022, save the date"
           style={{ height: "100%", width: "100%" }}
         />
+        <a
+          href="https://forms.gle/8yTEP1P76ov8gkUG9"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            position: "absolute",
+            bottom: "12%",
+            left: "50%",
+            transform: "translateX(-50%)",
+            textDecoration: "none",
+            background: "#e6d8cb",
+            padding: "14px",
+            textAlign: "center",
+            color: "#62625b",
+            fontSize: "smaller",
+          }}
+        >
+          Confirm now!
+        </a>
       </div>
     </div>
   );
